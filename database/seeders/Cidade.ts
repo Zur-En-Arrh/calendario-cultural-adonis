35a0a6363bc0f21b1c0f122ed7aa667b95f3b51c
Niuan Lucas <niuanlucasnicolau@gmail.com>
2023-02-28 22:22:26 -0300

    responsive filter

--- database/seeders/Cidade.ts
+++ database/seeders/Cidade.ts
@@ -5,36 +5,8 @@ export default class extends BaseSeeder {
   public async run () {
     await Cidade.createMany([
       {
-        nome:'Resende',
-        regiaoId: 1
-      },
-      {
-        nome:'Niterói',
-        regiaoId: 1
-      },
-      {
-        nome:'Japeri',
-        regiaoId: 1
-      },
-      {
-        nome:'Petrópolis',
-        regiaoId: 4
-      },
-      {
-        nome:'Mesquita',
-        regiaoId: 1
-      },
-      {
-        nome:'Rio de Janeiro',
-        regiaoId: 1
-      },
-      {
-        nome:'Belford Roxo',
-        regiaoId: 1
-      },
-      {
-        nome:'Nova Iguaçu',
-        regiaoId: 1
+        nome:'Paraty',
+        regiaoId: 8
       }
     ])
   }
